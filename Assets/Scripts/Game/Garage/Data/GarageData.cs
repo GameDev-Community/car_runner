6ce50a6ba6a5bbc6d33b4d62ffeff95d1b2bce2b
hooyamba <mvmse228@gmail.com>
2022-09-06 13:41:14 +0300

    серьезная работа в гараже Богдана

--- Assets/Scripts/Game/Garage/Data/GarageData.cs
+++ Assets/Scripts/Game/Garage/Data/GarageData.cs
@@ -1,4 +1,5 @@
 ﻿using Externals.Utils.Extentions;
+using Externals.Utils.Runtime;
 using Externals.Utils.SaveManager;
 using Game.Helpers;
 using System.Collections.Generic;
@@ -6,74 +7,84 @@ using System.IO;
 
 namespace Game.Garage
 {
-    public class GarageData : ISavable
+    public class UpgradeData : ISavable
     {
-        private class UpgradeData : ISavable
+        private UpgradeObject _upgradeObject;
+        private int _lastUpgradeTier;
+
+
+        public UpgradeData()
         {
-            private UpgradeObject _upgradeObject;
-            private int _lastUpgradeTier;
+        }
 
-
-            public UpgradeData()
-            {
-            }
-
-            public UpgradeData(UpgradeObject upgradeObject, int lastUpgradeTier)
-            {
-                _upgradeObject = upgradeObject;
-                _lastUpgradeTier = lastUpgradeTier;
-            }
-
-
-            public UpgradeObject UpgradeObject => _upgradeObject;
-            public int LastUpgradeTier { get => _lastUpgradeTier; set => _lastUpgradeTier = value; }
-
-
-            public void Load(BinaryReader br)
-            {
-                _upgradeObject = br.ReadGameDatabaseElement(Accessors.UpgradesDatabase);
-                _lastUpgradeTier = br.ReadInt32();
-
-            }
-
-            public void Save(BinaryWriter bw)
-            {
-                bw.WriteGameDatabaseElement(_upgradeObject);
-                bw.Write(_lastUpgradeTier);
-            }
+        public UpgradeData(UpgradeObject upgradeObject, int lastUpgradeTier)
+        {
+            _upgradeObject = upgradeObject;
+            _lastUpgradeTier = lastUpgradeTier;
         }
 
 
-        private readonly List<CarObject> _unlockedCars;
-        private readonly List<CarObject> _acquiredCars;
-        private readonly List<UpgradeData> _upgrades;
+        public UpgradeObject UpgradeObject => _upgradeObject;
+        public int LastUpgradeTier { get => _lastUpgradeTier; set => _lastUpgradeTier = value; }
+
+
+        public void Load(BinaryReader br)
+        {
+            _upgradeObject = br.ReadGameDatabaseElement(Accessors.UpgradesDatabase);
+            _lastUpgradeTier = br.ReadInt32();
+
+        }
+
+        public void Save(BinaryWriter bw)
+        {
+            bw.WriteGameDatabaseElement(_upgradeObject);
+            bw.Write(_lastUpgradeTier);
+        }
+    }
+
+    public class GarageData : ISavable
+    {
+        private readonly HashSet<CarObject> _unlockedCars;
+        private readonly HashSet<CarObject> _acquiredCars;
+        private readonly Dictionary<UpgradeObject, UpgradeData> _aquiredUpgrades;
 
 
         public GarageData()
         {
             _unlockedCars = new();
             _acquiredCars = new();
-            _upgrades = new();
+            _aquiredUpgrades = new();
         }
 
 
-        public List<CarObject> UnlockedCars => _unlockedCars;
-        public List<CarObject> AcquiredCars => _acquiredCars;
+        public HashSet<CarObject> UnlockedCars => _unlockedCars;
+        public HashSet<CarObject> AcquiredCars => _acquiredCars;
+        public Dictionary<UpgradeObject, UpgradeData> AcquiredUpgrades => _aquiredUpgrades;
 
 
         public void Save(BinaryWriter bw)
         {
             bw.WriteGameDatabaseElements(_unlockedCars);
             bw.WriteGameDatabaseElements(_acquiredCars);
-
+            bw.WriteSavablesT(_aquiredUpgrades.Values);
         }
 
         public void Load(BinaryReader br)
         {
             var cdb = Accessors.CarsDatabase;
-            br.ReadGameDatabaseElementsNonAlloc<CarObject>(cdb, _unlockedCars);
-            br.ReadGameDatabaseElementsNonAlloc<CarObject>(cdb, _acquiredCars);
-            br.ReadSavablesNonAlloc<UpgradeData>(_upgrades);
+            br.ReadGameDatabaseElementsNonAllocToCollection<CarObject>(cdb, _unlockedCars);
+            br.ReadGameDatabaseElementsNonAllocToCollection<CarObject>(cdb, _acquiredCars);
+            br.ReadSavablesNonAllocToRentedBuffer<UpgradeData>(out var pool, out var arr, out var c);
+
+            _aquiredUpgrades.EnsureCapacity(c);
+            var au = _aquiredUpgrades;
+
+            for (int i = -1; ++i < c;)
+            {
+                var item = arr[i];
+                au.Add(item.UpgradeObject, item);
+            }
         }
+
     }
 }
--- Assets/Scripts/Game/Garage/ScriptableObjects/CarObject.cs
+++ Assets/Scripts/Game/Garage/ScriptableObjects/CarObject.cs
@@ -1,6 +1,9 @@
 ﻿using DevourDev.Unity.ScriptableObjects;
 using Externals.Utils;
+using Externals.Utils.Runtime;
 using Externals.Utils.StatsSystem;
+using Game.Core.Car;
+using Game.Helpers;
 using UnityEngine;
 
 namespace Game.Garage
@@ -10,7 +13,7 @@ namespace Game.Garage
     {
         [SerializeField] private MetaInfo _metaInfo;
         [SerializeField] private StatDataRuntimeCreator[] _sourceStats;
-        [SerializeField] private GameObject _carPreviewPrefab;
+        [SerializeField] private CustomizableCar _carCustomizable;
         [SerializeField] private UpgradeObject[] _upgrades;
         [SerializeField] private int _cost;
 
@@ -18,11 +21,125 @@ namespace Game.Garage
         public MetaInfo MetaInfo => _metaInfo;
 
 
-        public object CreateCar()
+        /// <summary>
+        /// строит визуал машины в стоковой
+        /// комплектации (без апгрейдов)
+        /// </summary>
+        /// <returns></returns>
+        public GameObject CreateVisualsStock()
         {
-            //returns Instance
-            throw new System.NotImplementedException();
+            var custCar = InstantiateCar();
+
+            foreach (var u in _upgrades)
+            {
+                if (u.TryGetUpgrateTier(0, out var tier))
+                {
+                    tier.ApplyVisuals(custCar);
+                }
+            }
+
+
+            return custCar.gameObject;
+        }
+
+        /// <summary>
+        /// строит визуал машины в жопе
+        /// (с фулл последними апгрейдами)
+        /// </summary>
+        /// <returns></returns>
+        public GameObject CreateVisualsTop()
+        {
+            var custCar = InstantiateCar();
+
+            foreach (var u in _upgrades)
+            {
+                if (u.TryGetUpgrateTier(-1, out var tier))
+                {
+                    tier.ApplyVisuals(custCar);
+                }
+            }
+
+
+            return custCar.gameObject;
+        }
+
+        public GameObject CreateVisualsActive()
+        {
+            var gdata = Accessors.Player.GarageData;
+
+            if (!gdata.AcquiredCars.Contains(this))
+            {
+                UnityEngine.Debug.LogError(@"— Господин поручик — вы, говорят,
+                большой специалист в этих делах. Скажите, вон та дама в рот берет?" + "\n" +
+                @"— Которая ? — переспрашивает Ржевский." + "\n" +
+                @"— Вон та, в желтом платье." + "\n" +
+                @"— Ну, я со спины сказать не могу, — пожимает плечами поручик.
+                В этот момент дама поворачивается, поручик Ржевский пристально смотрит ей в лицо и говорит:" + "\n" +
+                @"— Эта — берет." + "\n" +
+                @"Корнет подходит к даме, они о чем - то беседуют и удаляются.Через некоторое время корнет возвращается, довольный:" + "\n" +
+                @"— Действительно, берет! Но как вы определили, поручик ? !" + "\n" +
+                @"— Послушайте, корнет, — веско произносит поручик Ржевский, — Рот есть — значит берёт!");
+                return CreateVisualsStock();
+            }
+
+            var custCar = InstantiateCar();
+
+            foreach (var u in _upgrades)
+            {
+                if (u.TryGetUpgrateTier(-1, out var tier))
+                {
+                    tier.ApplyVisuals(custCar);
+                }
+            }
+
+
+            return custCar.gameObject;
+        }
+
+
+        private CustomizableCar InstantiateCar()
+        {
+            return Instantiate(_carCustomizable);
+        }
+
+
+        public GameObject CreateCar()
+        {
+            var gdata = Accessors.Player.GarageData;
+
+            if (!gdata.AcquiredCars.Contains(this))
+            {
+                UnityEngine.Debug.Log("attempt to create not acquired car");
+
+                if (UnityEngine.Random.value < 0.5f)
+                    throw new System.InvalidOperationException();
+
+                return CreateVisualsTop();
+            }
+
+            var custCar = InstantiateCar();
+            var sc = Accessors.PlayerStats;
+
+            foreach (var u in _upgrades)
+            {
+                if (gdata.AcquiredUpgrades.TryGetValue(u, out var v) && v.LastUpgradeTier >= 0)
+                {
+                    if (u.TryGetUpgrateTier(v.LastUpgradeTier, out var tier))
+                    {
+                        tier.ApplyVisuals(custCar);
+                        tier.ApplyStats(sc);
+                    }
+                    else
+                    {
+                        DevourRuntimeHelpers.ThrowMessageModal("бляяяяяя", true);
+                    }
+                }
+
+            }
+
+
+            return custCar.gameObject;
         }
     }
-     
+
 }
